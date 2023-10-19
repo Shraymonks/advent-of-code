e4a90a79cfce94b0067631538bebe1c6c3864c17
Raymond Ha <raymond@shraymonks.com>
2023-10-19 17:09:37 -0400

    2015 Day 4

---
+++ 2015/04/part2.ts
@@ -0,0 +1,13 @@
+import { getInput } from '../utils.js';
+const { createHash } = await import('node:crypto');
+
+const secret = await getInput();
+
+let i = 1;
+while (
+  !createHash('md5').update(`${secret}${i}`).digest('hex').startsWith('000000')
+) {
+  i++;
+}
+
+console.log(i);
