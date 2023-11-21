76e73fa8a7478d980421618add9d1509392d3dc3
Raymond Ha <raymond@shraymonks.com>
2023-11-20 18:08:43 -0800

    2015 Day 13

---
+++ 2015/13/part1.ts
@@ -0,0 +1,57 @@
+import { getInput } from '../utils.js';
+
+const input = await getInput();
+const lines = input.split('\n');
+
+const happinessMap = new Map();
+
+for (const line of lines) {
+  const [, a, type, deltaString, b] = line.match(
+    /(.+) would (.+) (.+) happiness units by sitting next to (.+)\./
+  )!;
+
+  const delta = Number(deltaString);
+  const isGain = type === 'gain';
+
+  if (!happinessMap.has(a)) {
+    happinessMap.set(a, new Map());
+  }
+  happinessMap.get(a).set(b, (isGain ? 1 : -1) * delta);
+}
+
+const guests = new Set(happinessMap.keys());
+
+function permutations([first, ...rest]: string[]): string[][] {
+  if (first == null) {
+    return [];
+  }
+  if (rest.length === 0) {
+    return [[first]];
+  }
+  const result = [];
+  const subPermutations = permutations(rest);
+  for (const permutation of subPermutations) {
+    for (let i = 0; i <= permutation.length; i++) {
+      const copy = [...permutation];
+      copy.splice(i, 0, first);
+      result.push(copy);
+    }
+  }
+  return result;
+}
+
+const perms = permutations([...guests]);
+
+let maxHappiness = -Infinity;
+for (const permutation of perms) {
+  let happiness = 0;
+  for (let i = 0; i < permutation.length; i++) {
+    const guestHappiness = happinessMap.get(permutation[i]);
+    happiness +=
+      guestHappiness.get(permutation.at(i - 1)) +
+      guestHappiness.get(permutation.at((i + 1) % permutation.length));
+  }
+  maxHappiness = Math.max(maxHappiness, happiness);
+}
+
+console.log(maxHappiness);
