67b6aa4e2be326107ba24e5afcd1ab1b82aa398a
Raymond Ha <raymond@shraymonks.com>
2023-12-20 16:50:29 -0500

    2023 Day 20

---
+++ 2023/20/part2.mjs
@@ -0,0 +1,126 @@
+import { getInput } from '../utils.mjs';
+
+const input = await getInput();
+const lines = input.split('\n');
+
+class Module {
+  static #instances = new Map();
+  static #inputMap = new Map();
+  static highSent = 0;
+  static lowSent = 0;
+
+  constructor(name, outputs) {
+    this.name = name;
+    this.inputs = Module.#inputMap.get(name) ?? new Set();
+    this.outputs = new Set(outputs);
+  }
+
+  handlePulse(pulse) {
+    this.sendPulse(pulse);
+  }
+
+  sendPulse(pulse) {
+    for (const to of this.outputs) {
+      Module.#instances.get(to)?.handlePulse(pulse, this.name);
+    }
+    if (pulse) {
+      Module.highSent += this.outputs.size;
+    } else {
+      Module.lowSent += this.outputs.size;
+    }
+  }
+
+  addInput(name) {
+    this.inputs.add(name);
+  }
+
+  static add(name, outputs) {
+    for (const output of outputs) {
+      if (!Module.#inputMap.has(output)) {
+        Module.#inputMap.set(output, new Set());
+      }
+      Module.#inputMap.get(output).add(name);
+
+      if (Module.#instances.has(output)) {
+        Module.#instances.get(output).addInput(name);
+      }
+    }
+    const module = new this(name, outputs);
+    Module.#instances.set(name, module);
+    return module;
+  }
+
+  static getPressesForRx() {
+    const flipFlops = Module.#instances.get('broadcaster').outputs;
+    let total = 1;
+    for (const flipFlop of flipFlops) {
+      let n = '';
+      let current = Module.#instances.get(flipFlop);
+      while (current) {
+        const { outputs } = current;
+        const outputModules = Array.from(outputs, (output) =>
+          Module.#instances.get(output)
+        );
+        const hasConjunction = outputModules.some(
+          (output) => output instanceof Conjunction
+        );
+        n = (hasConjunction ? '1' : '0') + n;
+        current = outputModules.find((output) => output instanceof FlipFlop);
+      }
+      total *= parseInt(n, 2);
+    }
+    return total;
+  }
+}
+
+class Conjunction extends Module {
+  #memory = new Map();
+  #numOff = this.inputs.size;
+
+  handlePulse(pulse, from) {
+    const prevMemory = this.#memory.get(from);
+    this.#memory.set(from, pulse);
+    if (pulse !== prevMemory) {
+      this.#numOff += pulse ? -1 : 1;
+    }
+    this.sendPulse(this.#numOff > 0);
+  }
+
+  addInput(name) {
+    super.addInput(name);
+    this.#memory.set(name, false);
+    this.#numOff++;
+  }
+}
+
+class FlipFlop extends Module {
+  #on = false;
+
+  handlePulse(pulse) {
+    if (!pulse) {
+      this.#on = !this.#on;
+      this.sendPulse(this.#on);
+    }
+  }
+}
+
+for (const line of lines) {
+  const [nameInput, destinationsInput] = line.split(' -> ');
+  const outputs = destinationsInput.split(', ');
+  const type = nameInput[0];
+  const name = nameInput.slice(1);
+
+  switch (type) {
+    case '%':
+      FlipFlop.add(name, outputs);
+      break;
+    case '&':
+      Conjunction.add(name, outputs);
+      break;
+    case 'b':
+      Module.add(nameInput, outputs);
+      break;
+  }
+}
+
+console.log(Module.getPressesForRx());
