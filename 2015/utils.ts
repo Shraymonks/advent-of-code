f9b6518f366ebc8a68302afdbc23f467c7aa64bc
Raymond Ha <raymond@shraymonks.com>
2023-12-19 22:53:38 -0500

    Use readFile instead of open

--- 2015/utils.ts
+++ 2015/utils.ts
@@ -1,12 +1,10 @@
-import { open } from 'node:fs/promises';
+import { readFile } from 'node:fs/promises';
 
 export async function getInput(): Promise<string> {
   const inputPath = process.argv[2];
   if (!inputPath) {
     throw new Error('No input path provided.');
   }
-  const file = await open(inputPath);
-  const input = await file.readFile({ encoding: 'utf8' });
-  file.close();
+  const input = await readFile(inputPath, { encoding: 'utf8' });
   return input.trimEnd();
 }
